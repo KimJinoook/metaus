478e75c6028d8aad7555d88c38ac62a18fdbddd7
KimJinoook <99188096+KimJinoook@users.noreply.github.com>
2022-06-27 11:09:58 +0900

    파일업로드 최대용량 설정

--- metaus/src/main/java/com/metaus/configuration/MvcConfiguration.java
+++ metaus/src/main/java/com/metaus/configuration/MvcConfiguration.java
@@ -29,7 +29,7 @@ public class MvcConfiguration implements WebMvcConfigurer{
 		CommonsMultipartResolver multipartResolver 
 			= new CommonsMultipartResolver();
 		multipartResolver.setDefaultEncoding("UTF-8"); // 파일 인코딩 설정
-		multipartResolver.setMaxUploadSizePerFile(2 * 1024 * 1024); // 파일당 업로드 크기 제한 (2MB)
+		multipartResolver.setMaxUploadSizePerFile(1024 * 1024 * 1024); // 파일당 업로드 크기 제한 (2MB)
 		return multipartResolver;
 	}
 	
