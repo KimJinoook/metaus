03b55c59eadeffd43373265422c7fc1779e2768d
gnswp98 <105349816+gnswp98@users.noreply.github.com>
2022-06-29 18:08:42 +0900

    댓글 기능 추가 본인만 삭제기능 댓글 로그인 유효성

--- metaus/src/main/java/com/metaus/comment/model/CommentDAO.java
+++ metaus/src/main/java/com/metaus/comment/model/CommentDAO.java
@@ -1,13 +1,14 @@
 package com.metaus.comment.model;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface CommentDAO {
 	int insertComment(CommentVO vo);
-	List<CommentVO> selectComment(int boardNo);
+	List<Map<String, Object>> selectComment(int boardNo);
 	int deleteComment(int cmtNo);
 	int countComment(int boardNo);
 }
