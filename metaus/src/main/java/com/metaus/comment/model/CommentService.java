46825b7acdce5744693d642698d9395585d8da6a
gnswp98 <105349816+gnswp98@users.noreply.github.com>
2022-07-01 18:00:57 +0900

    대댓글 수정중

--- metaus/src/main/java/com/metaus/comment/model/CommentService.java
+++ metaus/src/main/java/com/metaus/comment/model/CommentService.java
@@ -6,8 +6,8 @@ import java.util.Map;
 public interface CommentService {
 	int insertComment(CommentVO vo);
 	List<Map<String, Object>> selectComment(int boardNo);
-	int deleteComment(int cmtNo);
 	int countComment(int boardNo);
 	int updateComment(CommentVO vo);
 	int reply(CommentVO vo);
+	public void deleteComment(Map<String, String> map);
 }
