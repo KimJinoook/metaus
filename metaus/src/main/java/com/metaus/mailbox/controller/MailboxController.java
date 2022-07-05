f27a08ff89e63b762c186ad24859706b31b1d51e
hanym <hanym@DESKTOP-QH4FUNT>
2022-07-05 17:06:57 +0900

    메세지 1조 메타어스 sql 0701.txt 수정 - msgView 수정, updateStarFlag 프로시져 추가
    별표 클릭 시 star_flag 업데이트 처리(ajax)

--- metaus/src/main/java/com/metaus/mailbox/controller/MailboxController.java
+++ metaus/src/main/java/com/metaus/mailbox/controller/MailboxController.java
@@ -1,5 +1,6 @@
 package com.metaus.mailbox.controller;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -14,6 +15,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.support.RequestContextUtils;
 
@@ -163,21 +165,56 @@ public class MailboxController {
 		int sentNo = mailboxService.findSentNo(memId);
 		logger.info("보낸 메세지 개수, sentNo={}", sentNo);
 		
+		int starNo = mailboxService.findStarNo(memId);
+		logger.info("별표 메세지 개수, starNo={}", starNo);
+		
+		int temporaryNo = mailboxService.findTemporaryNo(memId);
+		logger.info("임시 보관함 개수, temporaryNo={}", temporaryNo);
+		
+		int spamNo = mailboxService.findSpamNo(memId);
+		logger.info("임시 보관함 개수, spamNo={}", spamNo);
+		
+		int trashNo = mailboxService.findTrashNo(memId);
+		logger.info("임시 보관함 개수, trashNo={}", trashNo);
+		
 		model.addAttribute("receivedNo", receivedNo);
 		model.addAttribute("sentNo", sentNo);
+		model.addAttribute("starNo", starNo);
+		model.addAttribute("temporaryNo", temporaryNo);
+		model.addAttribute("spamNo", spamNo);
+		model.addAttribute("trashNo", trashNo);
 		
 		return "/mailbox/mailboxByFlag";
 	}
 	
 	@RequestMapping("/mailDetail")
-	public String mailDetail(@RequestParam(defaultValue = "0") int msgNo, ModelMap model) {
-		logger.info("메세지 상세 조회, 매개변수 msgNo={}", msgNo);
+	public String mailDetail(@RequestParam(defaultValue = "0") int msgaddNo, ModelMap model) {
+		logger.info("메세지 상세 조회, 매개변수 msgaddNo={}", msgaddNo);
 		
-		Map<String, Object> map = mailboxService.selectByMsgNo(msgNo);
+		Map<String, Object> map = mailboxService.selectByMsgAddNo(msgaddNo);
 		logger.info("메세지 상세 조회 결과, map={}", map);
 		
 		model.addAttribute("map", map);
 		
 		return "/mailbox/readMail";
+	}
+	
+	@RequestMapping("/starFlagUpdate")
+	@ResponseBody
+	public int starFlagUpdate(HttpSession session, @RequestParam boolean emptyFlag, @RequestParam String msgaddNo) {
+		String memId=(String) session.getAttribute("memId");
+		logger.info("별표 메세지 처리 파라미터, memId={}, msgaddNo={}, emptyFlag={}", memId, msgaddNo, emptyFlag);
+		
+		Map<String, String> map = new HashMap<>();
+		map.put("msgaddAdsee", memId);
+		map.put("emptyFlag", emptyFlag+"");
+		map.put("msgaddNo", msgaddNo);
+		
+		int cnt=mailboxService.updateStarFlag(map);
+		logger.info("별표 플래그 업데이트 결과, cnt={}", cnt);
+		
+		int starNo=mailboxService.findStarNo(memId);
+		
+		return starNo;
 	}
 }
